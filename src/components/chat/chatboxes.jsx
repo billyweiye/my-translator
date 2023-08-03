e5ece45a5f729335411605835fef10d137bf1557
billyweiye <31956826+billyweiye@users.noreply.github.com>
2023-08-03 05:06:57 +0000

    Update chatboxes.jsx

--- src/components/chat/chatboxes.jsx
+++ src/components/chat/chatboxes.jsx
@@ -3,16 +3,12 @@ import Button from "./button";
 import Chats from "./chats";
 import { LoadingChatLine } from "./chats";
 import ClearThread from "./buttonClear";
-import { useCookies } from 'react-cookie'
+import { useCookies } from "react-cookie";
 
-
-const COOKIE_NAME = 'nextjs-ai-chat-gpt3'
-
-
-
+const COOKIE_NAME = "nextjs-ai-chat-gpt3";
 
 export default function ChatBox() {
-  const [cookie, setCookie] = useCookies([COOKIE_NAME])
+  const [cookie, setCookie] = useCookies([COOKIE_NAME]);
   const [prompt, setPrompt] = useState("");
   const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -21,32 +17,26 @@ export default function ChatBox() {
   useEffect(() => {
     if (!cookie[COOKIE_NAME]) {
       // generate a semi random short id
-      const randomId = Math.random().toString(36).substring(7)
-      setCookie(COOKIE_NAME, randomId)
+      const randomId = Math.random().toString(36).substring(7);
+      setCookie(COOKIE_NAME, randomId);
     }
-  }, [cookie, setCookie])
+  }, [cookie, setCookie]);
 
   function handleChange(event) {
     setPrompt(event.target.value);
     //  console.log(event.target.value);
-
   }
-
-
 
   async function handleSubmitApi(input) {
     //  event.preventDefault();
 
     if (loading) {
-      alert("Please wait for the last response!")
-      return
-    }
-    else if (input) {
+      alert("Please wait for the last response!");
+      return;
+    } else if (input) {
       setLoading(true);
       history.push({ role: "user", content: input });
     } else return;
-
-
 
     try {
       const controller = new AbortController();
@@ -54,46 +44,46 @@ export default function ChatBox() {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         signal: controller.signal,
-        body: JSON.stringify({ activateStatus: activated, records: history, user: cookie[COOKIE_NAME] })
+        body: JSON.stringify({
+          activateStatus: activated,
+          records: history,
+          user: cookie[COOKIE_NAME],
+        }),
       });
 
       if (!activated) {
-        const data=await response.json();
+        const data = await response.json();
         console.log(data);
         if (data.activate) {
           setHistory((prevHistory) => [...prevHistory, data.result]);
-        }
-        else {
+        } else {
           setHistory((prevHistory) => [...prevHistory, data.result]);
         }
         setLoading(false);
         setActive(true);
-      }
-      else  {
+      } else {
         if (!response.ok) {
           setLoading(false);
-          alert('Something went wrong.');
+          alert("Something went wrong.");
           return;
         }
 
         const data = response.body;
 
-
         if (!data) {
           setLoading(false);
-          alert('Something went wrong.');
+          alert("Something went wrong.");
           return;
         }
-    
+
         const reader = data.getReader();
         const decoder = new TextDecoder();
         let done = false;
-        let code = '';
+        let code = "";
 
-  
         while (!done) {
           const { value, done: doneReading } = await reader.read();
           done = doneReading;
@@ -123,11 +113,7 @@ export default function ChatBox() {
             }
           });
         }
-
       }
-
-      
-
     } catch (error) {
       // Consider implementing your own error handling logic here
       //console.error(error);
@@ -136,60 +122,52 @@ export default function ChatBox() {
   }
 
   return (
+    <>
+      <div class="min-h-screen flex flex-col justify-items-stretch	">
+        <div className="rounded-xl h-80 border-zinc-100  border-4 mt-6">
+          {history.map((props, index) => (
+            <Chats key={index} role={props.role} content={props.content} />
+          ))}
+        </div>
 
-    <div className="rounded-2xl border-zinc-100  lg:border lg:p-6 justify-center items-center h-screen" >
-      {history.map((props, index) => (
-        <Chats key={index} role={props.role} content={props.content} />
-      ))}
-  
-  
-  
-      {/* {loading && <LoadingChatLine />} */}
-  
-      <div className="mt-6 flex clear-both">
-        <ClearThread type="submit" className="mr-4 flex-none" onClick={() => {
-          setHistory([]);
-          setPrompt("")
-        }} > Clear  </ClearThread>
-        <input
-          type="text"
-          aria-label="chat input"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
-          value={prompt}
-          onChange={handleChange}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSubmitApi(prompt)
-              setPrompt("")
-            }
-          }}
-        />
-        <Button
-          type="submit"
-          className="ml-4 flex-none"
-          onClick={() => {
-            handleSubmitApi(prompt)
-            setPrompt("")
-          }
-          }
-        >
-          Say
-        </Button>
+        <div class="flex mt-6 mb-8 justify-center self-end">
+          <ClearThread
+            type="submit"
+            className="mr-4 flex-none"
+            onClick={() => {
+              setHistory([]);
+              setPrompt("");
+            }}
+          >
+            {" "}
+            Clear{" "}
+          </ClearThread>
+          <input
+            type="text"
+            aria-label="chat input"
+            required
+            className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
+            value={prompt}
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSubmitApi(prompt);
+                setPrompt("");
+              }
+            }}
+          />
+          <Button
+            type="submit"
+            className="ml-4 flex-none"
+            onClick={() => {
+              handleSubmitApi(prompt);
+              setPrompt("");
+            }}
+          >
+            Say
+          </Button>
+        </div>
       </div>
-    </div>
-    
+    </>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
